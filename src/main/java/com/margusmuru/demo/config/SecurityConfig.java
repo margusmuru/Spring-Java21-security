853ba86052416f4b67f89abd104c6ca33b1e1303
Margus Muru <margusmuru@gmail.com>
2025-10-13 22:42:50 +0300

    password encryption

--- src/main/java/com/margusmuru/demo/config/SecurityConfig.java
+++ src/main/java/com/margusmuru/demo/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.margusmuru.demo.config;
 
-import com.margusmuru.demo.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -36,23 +35,10 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(userDetailsService);
 
         return provider;
     }
 
-
-    // Simple in-memory user details service for demonstration purposes
-  /*
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails user1 = User.withDefaultPasswordEncoder()
-        .username("user")
-        .password("password")
-        .roles("USER")
-        .build();
-    return new InMemoryUserDetailsManager(user1);
-  }
-   */
 }
--- src/main/java/com/margusmuru/demo/service/UserService.java
+++ src/main/java/com/margusmuru/demo/service/UserService.java
@@ -3,14 +3,17 @@ package com.margusmuru.demo.service;
 import com.margusmuru.demo.model.Users;
 import com.margusmuru.demo.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public Users registerUser(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
